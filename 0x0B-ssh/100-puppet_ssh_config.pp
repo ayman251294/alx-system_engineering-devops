af9fbff9c6387c136edc4d46e2702449cd216637
Ayman Nourredine <ayman251294@gmail.com>
2023-10-22 09:31:32 -0700

    new

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,15 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configu file
+
 # Ensure the SSH client configuration file exists
 file { '/etc/ssh/ssh_config':
-  ensure => 'file',
-}
+        ensure => present,
 
-# Ensure the SSH client is configured to use the private key ~/.ssh/school
-file_line { 'Set SSH Identity File':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh/school',
-  match   => '^#?[\s]*IdentityFile\s+~/.ssh/school',
-  require => File['/etc/ssh/ssh_config'],
-}
+content =>"
 
-# Ensure the SSH client is configured to refuse password authentication
-file_line { 'Disable Password Authentication':
-  path    => '/etc/ssh/ssh_config',
-  line    => '    PasswordAuthentication no',
-  match   => '^#?[\s]*PasswordAuthentication\s+no',
-  require => File['/etc/ssh/ssh_config'],
+        #SSH client configuration
+        host*
+        IdentityFile ~/.ssh/school
+        PasswordAuthentification no
+	",
 }
