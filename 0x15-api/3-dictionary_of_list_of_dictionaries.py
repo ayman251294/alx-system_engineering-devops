9960a1bb99d74bed534e806ab4da1f6292ab7e21
ayman251294 <ayman251294@gmail.com>
2023-10-23 14:20:54 -0700

    creat file 3-dictionary_of_list_of_dictionaries.py

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Using file 0-gather_data_from_an_API.py extend my Python script to export data in the JSON format"""
+import json
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicod.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+                } for t in requests.get(url + "todos",
+                    params={"userId": u.get("id")}).json()]
+                for u in users}, jsonfile)
